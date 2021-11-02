1b5cc9ddfb0d9f68376245dc655d94ef1d2ae144
Seneli Seneviratne <seneliseneviratne@cmail.carleton.ca>
2021-11-02 16:39:49 -0400

    general fonts and colors added

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,5 +1,90 @@
+//import React from "react";
+//import Fade from "react-reveal/Fade";
+//import data from "../content/footer";
+import "./styles/Footer.css";
+
 const Footer = () => {
-  return <div>I'm the 🦶er!</div>;
+  return (
+    <div className="footer" id="contact">
+      {/* <div>I'm the 🦶er!</div> */}
+
+      <div className="footer-column main">
+          <h3 className="blueprint-title">"image" Blueprint</h3>
+          <div>
+            <p className="stay-in-the-loop">stay in the loop</p>
+            <p> for loop of icons + links! </p>
+          </div>
+        </div>
+
+      <div className="body">
+
+        {/* 
+        <div className="footer-column">
+            <h4 className="blueprint-title">Home</h4>
+            <ul className="stay-in-the-loop">
+              <li>item</li> 
+              <li>item</li> 
+              <li>item</li> 
+              <li>item</li> 
+            </ul>
+        </div> */}
+
+        <div className="footer-column">
+            <h4 className="blueprint-title">About</h4>
+            <ul className="stay-in-the-loop">
+              <li>item</li> 
+              <li>item</li> 
+              <li>item</li> 
+              <li>item</li> 
+            </ul>
+        </div>
+
+        <div className="footer-column">
+            <h4 className="footer-title">Projects</h4>
+            <ul className="stay-in-the-loop">
+              <li>Current Projects</li> 
+              <li>Past Projects</li> 
+              <li>Apply</li> 
+            </ul>
+        </div>
+
+        <div className="footer-column">
+            <h4 className="footer-title">Team</h4>
+            <ul className="stay-in-the-loop">
+              <li>Beneficent</li> 
+              <li>Urban Minds</li> 
+              <li>Blueprint Website</li> 
+              <li>Join Us</li> 
+            </ul>
+        </div>
+
+        <div className="footer-column">
+            <h4 className="footer-title">Contact</h4>
+            <ul className="stay-in-the-loop">
+              <li>item</li> 
+              <li>item</li> 
+              <li>item</li> 
+              <li>item</li> 
+            </ul>
+        </div>
+
+        <div className="footer-column">
+            <h4 className="footer-title">Apply</h4>
+            <ul className="stay-in-the-loop">
+              <li>item</li> 
+              <li>item</li> 
+              <li>item</li> 
+              <li>item</li> 
+            </ul>
+        </div>
+        
+      </div>
+      <div className="made-with-luv">
+        Made with ❤️ by The Blueprint Team
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
+
