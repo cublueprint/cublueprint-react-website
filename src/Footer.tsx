b737f6f3e819ac4f8a6040e61c071874e7fa9bed
seneli <seneli.sene@gmail.com>
2021-11-09 14:37:06 -0500

    used css grid lol

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -9,94 +9,81 @@ const Footer = () => {
     <div className="footer" id="contact">
       {/* <div>I'm the 🦶er!</div> */}
 
-      <div className="footer-column main">
-          <h3 className="blueprint-title">"image" Blueprint</h3>
-          <div>
-            <p className="stay-in-the-loop">stay in the loop</p>
-            <div className="social-icons">
-                {footerImages.social.map((socialLink) => (
-                  <a
-                    href={socialLink.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    key={socialLink.key}
-                  >
-                    <img src={socialLink.img} alt="icons"></img>
-                  </a>
-                ))}
-              </div>
-              <a
-                className="return-button"
-                href="#"
-              >
-                Return to main site
-              </a>
+      <div className="body">
+
+
+        <div className="main-column">
+            <h3 className="blueprint-title">"image" Blueprint</h3>
+            <div>
+              <p className="stay-in-the-loop">Stay in the loop</p>
+              <div className="social-icons">
+                  {footerImages.social.map((socialLink) => (
+                    <a
+                      href={socialLink.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      key={socialLink.key}
+                    >
+                      <img src={socialLink.img} alt="icons"></img>
+                    </a>
+                  ))}
+                </div>
             </div>
         </div>
 
-      <div className="body">
+        <div className="nested">
+          <div className="footer-column">
+              <h4 className="blueprint-title">About</h4>
+              <ul className="stay-in-the-loop">
+                <li>item</li> 
+                <li>item</li> 
+                <li>item</li> 
+                <li>item</li> 
+              </ul>
+          </div>
 
-        {/* 
-        <div className="footer-column">
-            <h4 className="blueprint-title">Home</h4>
-            <ul className="stay-in-the-loop">
-              <li>item</li> 
-              <li>item</li> 
-              <li>item</li> 
-              <li>item</li> 
-            </ul>
-        </div> */}
+          <div className="footer-column">
+              <h4 className="footer-title">Projects</h4>
+              <ul className="stay-in-the-loop">
+                <li>Current Projects</li> 
+                <li>Past Projects</li> 
+                <li>Apply</li> 
+              </ul>
+          </div>
 
-        <div className="footer-column">
-            <h4 className="blueprint-title">About</h4>
-            <ul className="stay-in-the-loop">
-              <li>item</li> 
-              <li>item</li> 
-              <li>item</li> 
-              <li>item</li> 
-            </ul>
-        </div>
+          <div className="footer-column">
+              <h4 className="footer-title">Team</h4>
+              <ul className="stay-in-the-loop">
+                <li>Beneficent</li> 
+                <li>Urban Minds</li> 
+                <li>Blueprint Website</li> 
+                <li>Join Us</li> 
+              </ul>
+          </div>
 
-        <div className="footer-column">
-            <h4 className="footer-title">Projects</h4>
-            <ul className="stay-in-the-loop">
-              <li>Current Projects</li> 
-              <li>Past Projects</li> 
-              <li>Apply</li> 
-            </ul>
-        </div>
+          <div className="footer-column">
+              <h4 className="footer-title">Contact</h4>
+              <ul className="stay-in-the-loop">
+                <li>item</li> 
+                <li>item</li> 
+                <li>item</li> 
+                <li>item</li> 
+              </ul>
+          </div>
 
-        <div className="footer-column">
-            <h4 className="footer-title">Team</h4>
-            <ul className="stay-in-the-loop">
-              <li>Beneficent</li> 
-              <li>Urban Minds</li> 
-              <li>Blueprint Website</li> 
-              <li>Join Us</li> 
-            </ul>
-        </div>
-
-        <div className="footer-column">
-            <h4 className="footer-title">Contact</h4>
-            <ul className="stay-in-the-loop">
-              <li>item</li> 
-              <li>item</li> 
-              <li>item</li> 
-              <li>item</li> 
-            </ul>
-        </div>
-
-        <div className="footer-column">
-            <h4 className="footer-title">Apply</h4>
-            <ul className="stay-in-the-loop">
-              <li>item</li> 
-              <li>item</li> 
-              <li>item</li> 
-              <li>item</li> 
-            </ul>
+          <div className="footer-column">
+              <h4 className="footer-title">Apply</h4>
+              <ul className="stay-in-the-loop">
+                <li>item</li> 
+                <li>item</li> 
+                <li>item</li> 
+                <li>item</li> 
+              </ul>
+          </div>
         </div>
         
       </div>
+
       <div className="made-with-luv">
         Made with ❤️ by The Blueprint Team
       </div>
