9b80d324500a3cb2c2fc55bcaf7adb0b1c3d64c6
meerabal <meera.balsara@gmail.com>
2021-11-16 17:25:29 -0800

    added logo and reorganization

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { BlueprintIcon, StyledHeader, StyledHeaderList, StyledHeaderLink } from './content/json/header-assets';
 
 interface HeaderProps {
   links: string[];
@@ -8,8 +8,8 @@ interface HeaderProps {
 const Header = ({ links }: HeaderProps) => {
   return (
     <StyledHeader>
-      <>I'm the 🗣!</>
-      <StyledHeaderList> 
+      <BlueprintIcon />
+      <StyledHeaderList>
         {links.map((link) => (
           <StyledHeaderLink key={`l-${link}`}>
             <Link to={link}>{link}</Link>
@@ -19,47 +19,5 @@ const Header = ({ links }: HeaderProps) => {
     </StyledHeader>
   );
 };
-
-const StyledHeader = styled.div`
-  background-color: #3988FF;
-  color: #DFEFFF;
-  font-family: 'Poppins', sans-serif;
-  font-size: 16px;
-  font-weight: bold;
-  text-transform: uppercase;
-  
-  width: 100%;  
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-  z-index: 1;
-  position: -webkit-sticky;
-  position: sticky;
-  top: 0;
-  min-height: 8vh;
-`;
-
-const StyledHeaderList = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  gap: 3vw;
-`;
-
-
-const StyledHeaderLink = styled.div`
-  display: block;
-  margin-left: auto;
-  position: relative;
-  min-width: auto;
-  a {
-    text-decoration: none;
-    color: #DFEFFF;
-  }
-  a:hover {
-    color: #FFFFFF;
-  }
-`;
 
 export default Header;
